d54db12957ae995843856228a1836852cc85a778
brokendeveloper <fluccas09@gmail.com>
2025-10-07 21:39:54 -0300

    refactor: modularization of client side of application

--- client/client.py
+++ client/client.py
@@ -1,30 +1,60 @@
 import socket
 import threading
+from colorama import init, Fore, Style
 
-nickname = input("Escolha seu nome: ")
+init(autoreset=True)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
 
-def receive_messages():
-    while True:
+class Client:
+    def __init__(self, host='127.0.0.1', port=55555):
+        self.nickname = ""
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
+
+    def print_welcome_message(self):
+        print(Fore.CYAN + Style.BRIGHT + "=========================================")
+        print(Fore.CYAN + Style.BRIGHT + " Bem-vindo ao Chat Distribuído!")
+        print(Fore.CYAN + Style.BRIGHT + "=========================================")
+        print("Conectando ao servidor...")
+
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.client_socket.send(self.nickname.encode('utf-8'))
+                else:
+                    print(message)
+            except:
+                print(Fore.RED + "Você foi desconectado do servidor.")
+                self.client_socket.close()
+                break
+
+    def send_messages(self):
+        while True:
+            try:
+                message = input("")
+                full_message = f'{self.nickname}: {message}'
+                self.client_socket.send(full_message.encode('utf-8'))
+            except EOFError:
+                self.client_socket.close()
+                break
+            except:
+                break
+
+    def start(self):
+        self.print_welcome_message()
+        self.nickname = input("Escolha seu nome: ")
+
         try:
-            message = client.recv(1024).decode('utf-8')
-            if message == 'NICK':
-                client.send(nickname.encode('utf-8'))
-            else:
-                print(message)
+            self.client_socket.connect((self.host, self.port))
         except:
-            print("Ocorreu um erro! Desconectando...")
-            client.close()
-            break
+            print(Fore.RED + "Não foi possível conectar ao servidor.")
+            return
 
-def send_messages():
-    while True:
-        message = f'{nickname}: {input("")}'
-        client.send(message.encode('utf-8'))
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.daemon = True
+        receive_thread.start()
 
-receive_thread = threading.Thread(target=receive_messages)
-receive_thread.start()
-
-send_messages()
+        self.send_messages()
---
+++ client/main.py
@@ -0,0 +1,7 @@
+from client.client import Client
+
+if __name__ == "__main__":
+
+    chat_client = Client()
+
+    chat_client.start()
